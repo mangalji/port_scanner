f9e47b5c094aa17a89a81bf09c561f4314b8991c
mangalji <rajmangal03@gmail.com>
2025-12-07 13:56:09 +0530

    updated code

--- port_scanner_proj/scanner/consumers.py
+++ port_scanner_proj/scanner/consumers.py
@@ -4,6 +4,12 @@ import nmap
 from channels.generic.websocket import AsyncWebsocketConsumer
 # from channels.generic.websocket import WebsocketConsumer
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+thread_pool = ThreadPoolExecutor(max_workers=2000)
+
+
 
 class PortScanConsumer(AsyncWebsocketConsumer):
 
@@ -24,12 +30,18 @@ class PortScanConsumer(AsyncWebsocketConsumer):
         nm = nmap.PortScanner()
         host = socket.gethostbyname(ip)
 
+        loop = asyncio.get_running_loop()
+
         if data['mode'] == 'single':
             port = str(data['port'])
             
             # nm.scan(hosts=host, ports=port, arguments="-Pn")
-            await asyncio.to_thread(nm.scan, hosts=host, ports=port,  arguments="-Pn")
+            # await asyncio.to_thread(nm.scan, hosts=host, ports=port,  arguments="-Pn")
 
+            if int(port) > 65535:
+                await self.send(json.dumps({"error": f"{port} is invalid, please enter between 1-65535."}))
+                return 
+            await loop.run_in_executor(thread_pool,nm.scan,host,port,'-Pn -sV')
 
             if host not in nm.all_hosts():
                 # self.send(json.dumps({"error": f"{host} is not reachable"}))
@@ -37,8 +49,19 @@ class PortScanConsumer(AsyncWebsocketConsumer):
                 return 
 
             state = nm[host]['tcp'][int(port)]['state']
+            service = nm[host]['tcp'][int(port)].get('product','') + " " + nm[host]['tcp'][int(port)].get('version','')
+            service = service.strip() if service else 'unknown service'
 
-            print(f"host: {host}, port: {port}, status: {state}")
+            
+
+            #adding the os and service detecion feature.
+            # os_info = "Unknown OS"
+            # if 'osmatch' in nm[host] and len(nm[host]['osmatch']) > 0:
+            #     os_info = nm[host]['osmatch'][0]['name']  # Best OS guess
+
+            print(f"host: {host}, port: {port}, status: {state}, 'service':{service}")
+            
+            # await self.send(json.dumps({"os": os_info}))
 
             # self.send(json.dumps({
             #     "port":port,
@@ -47,12 +70,15 @@ class PortScanConsumer(AsyncWebsocketConsumer):
 
             await self.send(json.dumps({
                 "port":port,
-                "status": state
+                "status": state,
+                'service':service
             }))
 
         elif data["mode"] == 'all':
+            start_time = time.perf_counter()
             # nm.scan(hosts=host, ports=port, arguments="-Pn")
-            await asyncio.to_thread( nm.scan, hosts=host,ports='1-65535',arguments="-Pn")
+            # await asyncio.to_thread( nm.scan, hosts=host,ports='1-65535',arguments="-Pn")
+            await loop.run_in_executor(thread_pool,nm.scan,host,'1-65535','-Pn -sV')
 
             if host not in nm.all_hosts():
                 # self.send(json.dumps({"error": f"host {host} is not reachable."}))
@@ -60,12 +86,24 @@ class PortScanConsumer(AsyncWebsocketConsumer):
                 return 
 
             for port in nm[host]['tcp']:
+
                 state = nm[host]['tcp'][port]["state"]
+                version = nm[host]['tcp'][port].get('version', '')
+                product = nm[host]['tcp'][port].get('product', '')
+
+                service_info = f"{product} {version}".strip()
 
                 if state == 'open':
-                    print(f"host: {host}, port: {port}, status: {state}")
+                    print(f"host: {host}, port: {port}, status: {state}, service: {service_info}")
+                
                 # self.send(json.dumps({"port":port,"status":state}))
-                await self.send(json.dumps({"port":port,"status":state}))
+                await self.send(json.dumps({"port":port,"status":state,
+                                            "service":service_info}))
+
+            end_time = time.perf_counter()
+            total_time = round(end_time - start_time,3)
+            print("total time taken by this port scanner: ",total_time)
 
             # self.send(json.dumps({"done":True}))
-            await self.send(json.dumps({"done":True}))
+            await self.send(json.dumps({"done":True,"time":total_time}))
+
